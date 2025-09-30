d4900c368daf5fcd1f0ba38565248e09b7f94d50
Олег <om36325@gmail.com>
2025-09-30 21:17:47 +0300

    mylaba3.2

---
+++ laba3-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    double x;
+    double a;
+    double b;
+    double c;
+    double F;
+
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+    cout << "x = "; cin >> x;
+
+    if (x < 0 && b != 0){
+        F = -a * x * x + b;
+    }
+    if (x > 0 && b == 0){
+        F = (x / (x - c)) + 5.5;
+    }
+    if (!(x < 0 && b != 0) && !((x > 0 && b == 0))){
+        F = x / -c;
+    }
+
+    cout << endl;
+    cout << "1) F = " << F << endl;
+    
+    return 0;
+}
